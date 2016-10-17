908832bf3a620a366d3b3157779c0b7a3bb63f3f
Bryan Battershill <bryan.battershill@ocsbstudent.ca>
2016-10-17 14:40:03 -0400

    V3 or something

--- A3/src/BinarySearch.java
+++ A3/src/BinarySearch.java
@@ -6,15 +6,15 @@ public class BinarySearch {
 
 	public static int[] sortArray(int[] array){
 		
-		int[] sortedArray = new int[250];
+		int[] sortedArray = new int[array.length];
 		int lowestValue = 1000;
 		int indexedLowestValue = 0;
 		
-		for (int counter1 = 0; counter1 < 250; counter1++){
+		for (int counter1 = 0; counter1 < array.length; counter1++){
 			
 			lowestValue = 1000;
 			
-			for (int counter2 = 0; counter2 < 250; counter2++){
+			for (int counter2 = 0; counter2 < array.length; counter2++){
 				
 				if (array[counter2] <= lowestValue){
 					
@@ -32,7 +32,7 @@ public class BinarySearch {
 
 		boolean wasFound = false;
 		
-		for (int counter = 0; counter < 250; counter++){
+		for (int counter = 0; counter < array.length; counter++){
 			if (userInput == array[counter]){
 				
 				System.out.println("The number is placed at " + (counter + 1) + ".");
@@ -42,6 +42,14 @@ public class BinarySearch {
 		if (!wasFound){
 			System.out.println("That number could not be found.");
 		}
+	}
+	
+	public static int[] addNewValue(int[] array, int number){
+		int[] addedArray = new int [array.length];
+		addedArray = array.clone();
+		addedArray[addedArray.length - 1] = number;
+		addedArray = sortArray(addedArray);
+		return addedArray;
 	}
 	
 	public static void main(String[] args) {
@@ -62,18 +70,32 @@ public class BinarySearch {
 		System.out.println(Arrays.toString(sortedNumberSet));
 		
 		while (true){
-			System.out.println("What number would you like to find (1-1000)?");
+			System.out.println("What number would you like to find (1-1000)? Type 0 to add a new number.");
 			if (userInput.hasNextInt()){
 				userInputInt = userInput.nextInt();
 				if ((userInputInt>0)&&(userInputInt<1001)){
 					searchForVal(sortedNumberSet, userInputInt);
+				}
+				else if (userInputInt == 0){
+					System.out.println("What number would you like to add (1-1000)?");
+					if (userInput.hasNextInt()){
+						userInputInt = userInput.nextInt();
+						sortedNumberSet = Arrays.copyOf(sortedNumberSet, sortedNumberSet.length + 1);
+						sortedNumberSet = addNewValue(sortedNumberSet, userInputInt).clone();						
+						System.out.println("Added.");
+						System.out.println(Arrays.toString(sortedNumberSet));
+					}
+					else {
+						System.out.println("That is not valid.");
+						break;
+					}
 				}
 				else{
 					System.out.println("That is not a number between 1-1000.");
 				}
 			}
 			else {
-				System.out.println("That is not a number.");
+				System.out.println("That is not valid.");
 				break;
 			}
 		}
